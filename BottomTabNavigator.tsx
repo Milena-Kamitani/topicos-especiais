946c125a518c46ee40ceea7b64cd88572738c9c5
Milena <101467064+Milena-Kamitani@users.noreply.github.com>
2024-05-03 13:22:32 -0300

    Add files via upload

---
+++ BottomTabNavigator.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { AntDesign } from '@expo/vector-icons';
+import { MainStackNavigator, ToDoListStackNavigator } from './StackNavigator'
+
+export type TabParamList = {
+  HomeTab: undefined
+  TodoListTab: undefined
+}
+
+const Tab = createBottomTabNavigator<TabParamList>()
+
+const BottomTabNavigator = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName: 'home' | 'form' | undefined;
+
+          if (route.name === 'HomeTab') {
+            iconName = 'home'
+          } 
+
+          if (route.name === 'TodoListTab') {
+            iconName = 'form'
+          }
+
+          // You can return any component that you like here!
+          return <AntDesign name={iconName} size={size} color={color} />;
+        },
+      })}
+    >
+      <Tab.Screen
+        name="HomeTab"
+        options={{ title: 'Tab Squirtle' }}
+        component={MainStackNavigator}
+      />
+      <Tab.Screen
+        name="TodoListTab"
+        options={{ title: 'Tab Todo List' }}
+        component={ToDoListStackNavigator}
+      />
+    </Tab.Navigator>
+  )
+}
+
+export default BottomTabNavigator
